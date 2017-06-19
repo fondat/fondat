0bf9662c2a243c29edc291f4512fd0707df0573e
Paul Bryan <pbryan@anode.ca>
2017-06-18 22:00:20 -0700

    Test _rev support.

--- tests/test_file.py
+++ tests/test_file.py
@@ -1,24 +1,11 @@
 
+import roax.resource as r
 import roax.schema as s
 import unittest
 
 from roax.file import FileResourceSet
 from tempfile import TemporaryDirectory
 from uuid import uuid4
-
-class FooResourceSet(FileResourceSet):
-
-    schema = s.dict({
-        "_id": s.uuid(required=False),
-        "foo": s.str(),
-        "bar": s.int()
-    })
-
-    def __init__(self, dir):
-        super().__init__(dir)
-
-    def gen_id(self):
-        return uuid4()
 
 class TestFileResource(unittest.TestCase):
 
@@ -30,6 +17,21 @@ class TestFileResource(unittest.TestCase):
             fn(val)
 
     def test_crud(self):
+        
+        class FooResourceSet(FileResourceSet):
+
+            schema = s.dict({
+                "_id": s.uuid(required=False),
+                "foo": s.str(),
+                "bar": s.int()
+            })
+
+            def __init__(self, dir):
+                super().__init__(dir)
+
+            def gen_id(self):
+                return uuid4()
+
         with TemporaryDirectory() as dir:
             rs = FooResourceSet(dir)
             r1 = { "foo": "hello", "bar": 1 }
@@ -42,6 +44,43 @@ class TestFileResource(unittest.TestCase):
             r2 = rs.read(_id)
             self.assertEqual(r1, r2) 
             rs.delete(_id)
+            self.assertEqual(rs.query_ids(), [])
+
+    def test_rev(self):
+
+        class FooResourceSet(FileResourceSet):
+
+            schema = s.dict({
+                "_id": s.str(required=False),
+                "_rev": s.int(required=False),
+                "foo": s.str()
+            })
+
+            def __init__(self, dir):
+                super().__init__(dir)
+
+            def gen_id(self):
+                return str(uuid4())
+
+            def gen_rev(self, _doc):
+                print("gen_rev was here")
+                _rev = _doc.get("_rev")
+                _rev = _rev + 1 if _rev else 1
+                return _rev
+
+        with TemporaryDirectory() as dir:
+            rs = FooResourceSet(dir)
+            _doc = { "foo": "bar" }
+            result = rs.create(_doc)
+            _id = result["_id"]
+            _rev = result["_rev"]
+            self.assertEqual(_rev, 1)
+            _doc = { "foo": "qux" }
+            result = rs.update(_id, _doc, 1)
+            print(str(result))
+            self.assertEqual(result["_rev"], 2)
+            with self.assertRaises(r.PreconditionFailed):
+                rs.update(_id, _doc, 3)
 
 if __name__ == "__main__":
     unittest.main()
