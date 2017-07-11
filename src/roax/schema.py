8d92ebcba03dfbdbf36d428eecfa412a6dc31ca5
Paul Bryan <pbryan@anode.ca>
2017-07-11 13:02:28 -0700

    Cleanup and rename to *of.

--- src/roax/schema.py
+++ src/roax/schema.py
@@ -6,25 +6,17 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-from roax._schema import _type as type
-from roax._schema import _dict as dict
-from roax._schema import _list as list
-from roax._schema import _str as str
-from roax._schema import _int as int
-from roax._schema import _float as float
-from roax._schema import _bool as bool
-from roax._schema import _bytes as bytes
-from roax._schema import _none as none
-from roax._schema import _datetime as datetime
-from roax._schema import _uuid as uuid
-from roax._schema import _all as all
-from roax._schema import _any as any
-from roax._schema import _one as one
+import roax._schema as _schema
 
-from roax._schema import call
-from roax._schema import validate
-from roax._schema import SchemaError
+def _export(*args):
+    for name in args:
+        globals()[name] = getattr(_schema, name, None) or getattr(_schema, "_" + name)
+        __all__.append(name)
 
-__all__ = [ "type", "dict", "list", "str", "int", "float", "bool",
-        "bytes", "none", "datetime", "uuid", "all", "any", "one",
-        "call", "validate", "SchemaError" ]
+__all__ = []
+
+_export("SchemaError")
+_export("type", "dict", "list", "str", "int", "float", "bool", "bytes", "none")
+_export("datetime", "uuid")
+_export("allof", "anyof", "oneof")
+_export("call", "validate")
