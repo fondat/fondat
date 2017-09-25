aeeae36f3eca363adb8e4ddedf2c9fe02887bdf9
Paul Bryan <pbryan@anode.ca>
2017-09-24 19:30:09 -0700

    Drop Python 3.4 support.

--- setup.py
+++ setup.py
@@ -32,7 +32,6 @@ classifiers = [
     "Development Status :: 2 - Pre-Alpha",
     "Intended Audience :: Developers",
     "License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)",
-    "Programming Language :: Python :: 3.4",
     "Programming Language :: Python :: 3.5",
     "Programming Language :: Python :: 3.6",
     "Topic :: Internet :: WWW/HTTP :: WSGI :: Application"
