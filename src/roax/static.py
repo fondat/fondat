199695cc2f0d48d4e9c40c4ba69b34a1b54f2755
Paul Bryan <pbryan@anode.ca>
2018-04-16 17:44:35 -0700

    New static content resource.

---
+++ src/roax/static.py
@@ -0,0 +1,30 @@
+"""Module to provide read access to static content."""
+
+# Copyright © 2018 Paul Bryan.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+from roax.resource import Resource, operation
+
+class StaticResource(Resource):
+    """
+    TODO: Description.
+    """
+
+    def __init__(self, content, schema, name=None, description=None):
+        """
+        TODO: Description.
+        
+        content: the static content to return in a read operation.
+        schema: the schema of the static content.
+        """
+        super().__init__(name, description)
+        self.content = content
+        self.schema = schema
+        self.read = operation(params={}, returns=self.schema)(self.read)
+
+    def read(self):
+        """Read the static resource."""
+        return content
