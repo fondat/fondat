05f49cf457ec119c5a5808b1b657b5071f1519ef
Paul Bryan <pbryan@anode.ca>
2018-05-08 22:10:43 -0700

    Add tests to CLI module.

---
+++ tests/test_cli.py
@@ -0,0 +1,47 @@
+
+import json
+import roax.schema as s
+import unittest
+
+from io import BytesIO, StringIO
+from roax.cli import CLI
+from roax.resource import BadRequest, Resource, operation
+
+class TestResource(Resource):
+
+    @operation(params={"_body": s.bytes(format="binary")}, returns=s.dict({"id": s.str()}))
+    def create(self, _body):
+        if _body != b"hello_body":
+            raise BadRequest("_body not hello_body")
+        return {"id": "foo"}
+
+    @operation(type="action", params={"a": s.int(), "b": s.str()}, returns=s.str())
+    def foo(self, a, b):
+        return "hoot"
+
+cli = CLI(err=None)
+cli.register("test", TestResource())
+
+class TestCLI(unittest.TestCase):
+
+    def test_cli_params(self):
+        line = "test foo --a=1 --b=abc"
+        out = StringIO()
+        self.assertEqual(cli.process(line, out=out), True)
+        self.assertEqual(out.getvalue(), "hoot")
+
+    def test_cli_create_binary_body_success(self):
+        line = "test create"
+        inp = BytesIO(b"hello_body")
+        out = StringIO()
+        self.assertEqual(cli.process(line, inp=inp, out=out), True)
+        self.assertEqual(json.loads(out.getvalue()), {"id": "foo"})
+
+    def test_cli_create_binary_body_failure(self):
+        line = "test create"
+        inp = BytesIO(b"not_a_match")
+        out = StringIO()
+        self.assertEqual(cli.process(line, inp=inp, out=out), False)
+
+if __name__ == "__main__":
+    unittest.main()
