3b827ec371652a5198351c6e5913cf2cbd80441b
Paul Bryan <pbryan@anode.ca>
2018-05-24 20:36:22 -0700

    Increase test coverage.

---
+++ tests/test_openapi.py
@@ -0,0 +1,40 @@
+
+import roax.schema as s
+import unittest
+
+from roax.openapi import OpenAPIResource 
+from roax.resource import Resource, operation
+from roax.wsgi import App
+
+
+schema = s.dict({
+    "a": s.list(s.str()),
+    "b": s.set(s.str()),
+    "c": s.int(),
+    "d": s.float(),
+    "e": s.bool(),
+    "f": s.bytes(),
+    "g": s.datetime(),
+    "h": s.uuid(),
+})
+
+
+class TestResource(Resource):
+
+    @operation(type="action", params={"_body": schema}, security=[])
+    def test(self):
+        pass
+
+app = App("/", "Title", "1.0")
+app.register("/test", TestResource())
+
+
+class TestOpenAPI(unittest.TestCase):
+
+    def test_openapi_no_errors(self):
+        resource = OpenAPIResource(app)
+        resource.read()
+
+
+if __name__ == "__main__":
+    unittest.main()
