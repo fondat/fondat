bb92e3a6cd733a933bdc2fafb403a2371545203f
Paul Bryan <pbryan@anode.ca>
2018-10-21 15:00:25 -0700

    Add Python 3.7 to classifiers.

--- setup.py
+++ setup.py
@@ -34,6 +34,7 @@ classifiers = [
     "License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)",
     "Programming Language :: Python :: 3.5",
     "Programming Language :: Python :: 3.6",
+    "Programming Language :: Python :: 3.7",
     "Topic :: Internet :: WWW/HTTP :: WSGI :: Application"
 ]
 
