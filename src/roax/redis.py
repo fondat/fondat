9396ae674fd4a1ab38c7a52caa881ae12dd1eb12
Paul Bryan <pbryan@anode.ca>
2018-11-25 16:33:39 -0800

    New Redis resource.

---
+++ src/roax/redis.py
@@ -0,0 +1,57 @@
+"""Module to store resource items in Redis."""
+
+# Copyright © 2018 Paul Bryan.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+import json
+import redis
+
+from .resource import Conflict, NotFound, Resource
+
+
+class RedisResource(Resource):
+
+    def __init__(self, connection_pool, schema=None, id_schema=None,
+                 evict=False, ttl=None, name=None, description=None):
+        super().__init__(name, description)
+        self.redis = redis.Redis(connection_pool=connection_pool)
+        self.schema = schema or self.schema
+        self.id_schema = id_schema or getattr(self, "id_schema", None) or self.schema.properties["id"]
+        self.evict = evict
+        self.ttl = ttl
+
+    def _encode_id(self, id):
+        return self.name + "." + self.id_schema.json_encode(id)
+
+    def _encode_body(self, body):
+        return json.dumps(self.schema.json_encode(body)).encode()
+
+    def _decode_body(self, body):
+        return self.schema.json_decode(json.loads(body.decode()))
+
+    def create(self, id, _body):
+        px = int(self.ttl * 1000) if self.ttl else None
+        if not self.redis.set(self._encode_id(id), self._encode_body(_body), nx=True, px=px):
+            raise Conflict("{} item already exists".format(self.name))
+        return {"id": id}
+
+    def read(self, id):
+        item = self.redis.get(self._encode_id(id))
+        if item is None:
+            raise NotFound("{} item not found".format(self.name))
+        return self._decode_body(item)
+
+    def update(self, id, _body):
+        str_id = self._encode_id(id)
+        ttl = self.redis.ttl(str_id)
+        if ttl == -1:
+            ttl = None
+        if (ttl and ttl <= 0) or not self.redis.set(str_id, self._encode_body(_body), ex=ttl, xx=True):
+            raise NotFound("{} item not found".format(self.name))
+
+    def delete(self, id):
+        if not self.redis.delete(self._encode_id(id)):
+            raise NotFound("{} item not found".format(self.name))
---
+++ tests/test_redis.py
@@ -0,0 +1,82 @@
+
+import roax.resource as r
+import roax.schema as s
+import unittest
+
+from redis import ConnectionPool, Redis
+from roax.redis import RedisResource
+from roax.resource import BadRequest, Conflict, NotFound, operation
+from time import sleep
+from uuid import uuid4
+
+
+_schema = s.dict({
+    "id": s.uuid(required=False),
+    "foo": s.str(),
+    "bar": s.int(required=False),
+})
+
+
+class TestRedisResource(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        pool = ConnectionPool()
+        cls.r = Redis(connection_pool=pool)
+        cls.r.flushdb()
+        cls.rr = RedisResource(connection_pool=pool, schema=_schema, id_schema=_schema.properties["id"])
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.r.flushdb() 
+
+    def test_create_conflict(self):
+        id = uuid4()
+        self.rr.create(id, {"id": id, "foo": "bar"})
+        with self.assertRaises(Conflict):
+            self.rr.create(id, {"id": id, "foo": "baz"})
+
+    def test_read(self):
+        id = uuid4()
+        value = {"id": id, "foo": "bar"}
+        response = self.rr.create(id, value)
+        self.assertEqual(response["id"], id)
+        self.assertEqual(self.rr.read(id), value)
+
+    def test_read_notfound(self):
+        id = uuid4()
+        with self.assertRaises(NotFound):
+            self.rr.read(id)
+
+    def test_update(self):
+        id = uuid4()
+        value = {"id": id, "foo": "bar"}
+        self.rr.create(id, value)
+        value = {"id": id, "foo": "qux"}
+        self.rr.update(id, value)
+        self.assertEqual(self.rr.read(id), value)
+
+    def test_update_notfound(self):
+        id = uuid4()
+        value = {"id": id, "foo": "bar"}
+        with self.assertRaises(NotFound):
+            self.rr.update(id, value)
+
+    def test_delete_notfound(self):
+        id = uuid4()
+        with self.assertRaises(NotFound):
+            self.rr.delete(id)
+
+    def test_ttl(self):
+        xr = RedisResource(connection_pool=ConnectionPool(),
+                  schema=_schema, id_schema=_schema.properties["id"], ttl=.001)
+        id = uuid4()
+        value = {"id": id, "foo": "bar"}
+        xr.create(id, value)
+        xr.read(id)
+        sleep(.002)
+        with self.assertRaises(NotFound):
+            read = xr.read(id)
+
+if __name__ == "__main__":
+    unittest.main()
