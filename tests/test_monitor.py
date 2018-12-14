b7fe6b07be1b83aba5ede5349a580a54255ce201
Paul Bryan <pbryan@anode.ca>
2018-12-13 22:46:36 -0800

    Add test cases.

--- tests/test_monitor.py
+++ tests/test_monitor.py
@@ -1,11 +1,16 @@
-import unittest
-
 import roax.monitor as m
 import roax.schema as s
+import unittest
+
+from datetime import datetime, timedelta, timezone
+
 
 _tags = {"name": "test"}
 
 _dt = lambda string: s.datetime().str_decode(string)
+
+_now = lambda: datetime.now(tz=timezone.utc)
+
 
 class TestMonitor(unittest.TestCase):
 
@@ -27,7 +32,7 @@ class TestMonitor(unittest.TestCase):
         m.monitor.record(_tags, _dt("2018-12-01T00:01:05Z"), _type, 50)
         series = m.monitor.series["test"]
         self.assertEqual(series.type, _type)
-        data = m.monitor.series["test"].data
+        data = series.data
         self.assertEqual(len(data), 2)
         dp = data[0]
         self.assertEqual(dp.timestamp, _dt("2018-12-01T00:00:00Z"))
@@ -51,7 +56,7 @@ class TestMonitor(unittest.TestCase):
         m.monitor.record(_tags, _dt("2018-12-01T00:01:59Z"), _type, 50)
         series = m.monitor.series["test"]
         self.assertEqual(series.type, _type)
-        data = m.monitor.series["test"].data
+        data = series.data
         self.assertEqual(len(data), 2)
         dp = data[0]
         self.assertEqual(dp.timestamp, _dt("2018-12-01T00:00:00Z"))
@@ -81,7 +86,7 @@ class TestMonitor(unittest.TestCase):
         m.monitor.record(_tags, _dt("2018-12-01T00:01:59Z"), _type, 50)
         series = m.monitor.series["test"]
         self.assertEqual(series.type, _type)
-        data = m.monitor.series["test"].data
+        data = series.data
         self.assertEqual(len(data), 2)
         dp = data[0]
         self.assertEqual(dp.timestamp, _dt("2018-12-01T00:00:00Z"))
@@ -89,6 +94,45 @@ class TestMonitor(unittest.TestCase):
         dp = data[1]
         self.assertEqual(dp.timestamp, _dt("2018-12-01T00:01:00Z"))
         self.assertEqual(dp.value, 150)
+
+    def test_truncation(self):
+        _type = "absolute"
+        m.monitor.track("test", _type, _tags, 3, 60)
+        start = _dt("2018-12-01T00:00:00Z")
+        m.monitor.record(_tags, _dt("2018-12-01T00:00:00Z"), _type, 1)
+        m.monitor.record(_tags, _dt("2018-12-01T00:01:00Z"), _type, 2)
+        m.monitor.record(_tags, _dt("2018-12-01T00:02:00Z"), _type, 3)
+        m.monitor.record(_tags, _dt("2018-12-01T00:03:00Z"), _type, 4)
+        data = m.monitor.series["test"].data
+        self.assertEqual(len(data), 3)
+        self.assertEqual(data[0].timestamp, _dt("2018-12-01T00:01:00Z"))
+
+    def test_untracked_tag_ignore(self):
+        _type = "absolute"
+        m.monitor.track("test", _type, _tags, 60, 60)
+        m.monitor.record({"another": "tag"}, _dt("2018-12-01T00:00:00Z"), _type, 1)
+        self.assertEqual(len(m.monitor.series["test"].data), 0)
+
+    def test_backdate_preserve_order(self):
+        _type = "absolute"
+        m.monitor.track("test", _type, _tags, 60, 60)
+        m.monitor.record(_tags, _dt("2018-12-01T00:01:00Z"), _type, 1)
+        m.monitor.record(_tags, _dt("2018-12-01T00:00:00Z"), _type, 2)
+        series = m.monitor.series["test"]
+        self.assertEqual(series.type, _type)
+        data = series.data
+        self.assertEqual(len(data), 2)
+        self.assertEqual(data[0].timestamp, _dt("2018-12-01T00:00:00Z"))
+
+    def test_type_mismatch_error(self):
+        m.monitor.track("test", "absolute", _tags, 60, 60)
+        with self.assertRaises(ValueError):
+            m.monitor.record(_tags, _dt("2018-12-01T00:01:00Z"), "gauge", 1)
+        
+    def test_type_future_error(self):
+        m.monitor.track("test", "absolute", _tags, 60, 60)
+        with self.assertRaises(ValueError):
+            m.monitor.record(_tags, _now() + timedelta(seconds=1), "gauge", 1)
 
 
 if __name__ == "__main__":
