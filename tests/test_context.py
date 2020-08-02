420ba5a188f5aff0cd42129e7d2466bc1744ca36
Paul Bryan <pbryan@anode.ca>
2020-08-01 17:06:23 -0700

    Simprify.

--- tests/test_context.py
+++ tests/test_context.py
@@ -5,22 +5,26 @@ import roax.context as context
 import time
 
 
+def count(iterable):
+    return sum(1 for _ in iterable)
+
+
 def test_basic():
-    assert sum(1 for _ in context.find()) == 0
+    assert count(context.find()) == 0
     with context.push(context="foo", value=1):
         with context.push(context="foo", value=2):
             assert context.last(context="root") is not None
-            assert sum(1 for _ in context.find()) == 3
-            assert sum(1 for _ in context.find()) == 3
+            assert count(context.find()) == 3
+            assert count(context.find()) == 3
             assert context.first(context="foo")["value"] == 1
             assert context.last(context="foo")["value"] == 2
-            assert sum(1 for _ in context.find(context="foo")) == 2
-    assert sum(1 for _ in context.find()) == 0
+            assert count(context.find(context="foo")) == 2
+    assert count(context.find()) == 0
 
 
 def test_thread_isolation():
     def thread(seed):
-        assert sum(1 for _ in context.find()) == 0  # threads do not inherit from invoker
+        assert count(context.find()) == 0  # threads do not inherit from invoker
         for n in range(0, 10):
             with context.push(context="thread1", seed=seed):
                 time.sleep(0.01)
@@ -29,22 +33,22 @@ def test_thread_isolation():
                 with context.push(context="thread2", seed=seed2):
                     time.sleep(0.01)
                     assert context.last(context="thread2")["seed"] == seed2
-        assert sum(1 for _ in context.find()) == 0
+        assert count(context.find()) == 0
 
-    assert sum(1 for _ in context.find()) == 0
+    assert count(context.find()) == 0
     with context.push(context="main"):
-        assert sum(1 for _ in context.find()) == 2  # main and root
+        assert count(context.find()) == 2  # main and root
         with concurrent.futures.ThreadPoolExecutor() as executor:
             futures = [executor.submit(thread, s) for s in [1000, 2000, 3000]]
             for future in concurrent.futures.as_completed(futures):
                 future.result()
-        assert sum(1 for _ in context.find()) == 2
-    assert sum(1 for _ in context.find()) == 0
+        assert count(context.find()) == 2
+    assert count(context.find()) == 0
 
 
 def test_coroutine_isolation():
     async def coro(seed):
-        assert sum(1 for _ in context.find()) == 2  # coroutines inherit from caller
+        assert count(context.find()) == 2  # coroutines inherit from caller
         for n in range(0, 10):
             with context.push(context="coro1", seed=seed):
                 await asyncio.sleep(0.01)
@@ -53,15 +57,15 @@ def test_coroutine_isolation():
                 with context.push(context="coro2", seed=seed2):
                     await asyncio.sleep(0.01)
                     assert context.last(context="coro2")["seed"] == seed2
-        assert sum(1 for _ in context.find()) == 2
+        assert count(context.find()) == 2
 
     async def run():
-        assert sum(1 for _ in context.find()) == 0
+        assert count(context.find()) == 0
         with context.push(context="main"):
-            assert sum(1 for _ in context.find()) == 2  # main and root
+            assert count(context.find()) == 2  # main and root
             coroutines = [coro(s) for s in [1000, 2000, 3000]]
             await asyncio.gather(*coroutines)
-            assert sum(1 for _ in context.find()) == 2
-        assert sum(1 for _ in context.find()) == 0
+            assert count(context.find()) == 2
+        assert count(context.find()) == 0
 
     asyncio.run(run())
