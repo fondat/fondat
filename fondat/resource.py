027d140f8085c6d7dc930aa72e8a35e7df10d586
Paul Bryan <pbryan@anode.ca>
2020-08-18 23:39:34 -0700

    Support async operations, prepare for GraphQL support.

--- fondat/resource.py
+++ fondat/resource.py
@@ -1,43 +1,43 @@
-"""Module to implement resources."""
+"""
+Module to implement resources.
 
-import datetime
-import importlib
+A resource is an addressible object that exposes operations through a uniform
+set of methods.
+
+
+A resource class contains operation methods, each decorated with the
+@operation decorator.
+"""
+
+import asyncio
+import functools
 import inspect
-import roax.context as context
-import roax.monitor
-import roax.schema
-import threading
+import fondat.context as context
+import fondat.monitor as monitor
+import fondat.schema as schema
 import wrapt
-
-
-_now = lambda: datetime.datetime.now(tz=datetime.timezone.utc)
-
-
-class _Operation:
-    """A resource operation."""
-
-    def __init__(self, attrs):
-        for k in attrs:
-            self.__setattr__(k, attrs[k])
-
-    def call(self, **kwargs):
-        """Call the resource operation with keyword arguments."""
-        return getattr(self.resource, self.function)(**kwargs)
 
 
 class ResourceError(Exception):
     """
-    Base class for all resource errors.
+    Base class for resource errors.
 
     Parameters:
     • detail: Textual description of the error.
-    • code: HTTP status most closely associated with the error.
+    • code: HTTP status associated with the error.
     """
 
     def __init__(self, detail=None, code=None):
-        super().__init__(self, detail)
-        self.detail = detail or getattr(self, "detail", "Internal Server Error")
-        self.code = code or getattr(self, "code", 500)
+        if detail is not None:
+            self.detail = detail
+        if code is not None:
+            self.code = code
+        try:
+            self.detail, self.code
+        except AttributeError as ae:
+            raise ValueError(ae)
+
+        super().__init__(self.detail)
 
     def __str__(self):
         return self.detail
@@ -46,191 +46,49 @@ class ResourceError(Exception):
 class BadRequest(ResourceError):
     """Raised if the request is malformed."""
 
-    code, detail = 400, "Bad Request"
+    detail, code = "Bad Request", 400
 
 
 class Unauthorized(ResourceError):
     """Raised if the request lacks valid authentication credentials."""
 
-    code, detail = 401, "Unauthorized"
+    detail, code = "Unauthorized", 401
 
 
 class Forbidden(ResourceError):
     """Raised if authorization to the resource is refused."""
 
-    code, detail = 403, "Forbidden"
+    detail, code = "Forbidden", 403
 
 
 class NotFound(ResourceError):
     """Raised if the resource could not be found."""
 
-    code, detail = 404, "Not Found"
+    detail, code = "Not Found", 404
 
 
 class OperationNotAllowed(ResourceError):
     """Raised if the resource does not allow the requested operation."""
 
-    code, detail = 405, "Operation Not Allowed"
+    detail, code = "Operation Not Allowed", 405
 
 
 class Conflict(ResourceError):
     """Raised if there is a conflict with the current state of the resource."""
 
-    code, detail = 409, "Conflict"
+    detail, code = "Conflict", 409
 
 
 class PreconditionFailed(ResourceError):
     """Raised if the revision provided does not match the current resource."""
 
-    code, detail = 412, "Precondition Failed"
+    detail, code = "Precondition Failed", 412
 
 
 class InternalServerError(ResourceError):
     """Raised if the server encountered an unexpected condition."""
 
-    code, detail = 500, "Internal Server Error"
-
-
-class Resource:
-    """
-    Base class for a resource.
-    
-    Parameters and attributes:
-    • name: Short name of the resource.  [class name in lower case]
-    • description: Short description of the resource.  [resource docstring]
-    • security: Security requirements for resource operations.
-
-    Description be defined in a class variable.
-
-    Security requirements can be defined as a class variable. All resource
-    operations inherit the resource's security requirements unless overrriden
-    in the operation itself. 
-    """
-
-    def __init__(self, name=None, description=None, security=None):
-        super().__init__()
-        self.name = name or getattr(self, "name", type(self).__name__.lower())
-        self.description = (
-            description
-            or getattr(self, "description", None)
-            or self.__doc__
-            or self.__class__.__name__
-        )
-        self.security = (
-            security if security is not None else getattr(self, "security", None)
-        )
-        self.operations = {}
-        for function in (
-            attr for attr in (getattr(self, nom) for nom in dir(self)) if callable(attr)
-        ):
-            try:
-                operation = function._roax_operation_
-            except:
-                continue  # ignore undecorated functions
-            self._register_operation(**operation)
-
-    def _register_operation(self, **operation):
-        """Register a resource operation."""
-        name = operation["name"]
-        if name in self.operations:
-            raise ValueError(f"{name} operation name already registered")
-        security = (
-            operation["security"]
-            if operation["security"] is not None
-            else self.security
-        )
-        self.operations[name] = _Operation(
-            {**operation, "resource": self, "security": security}
-        )
-
-
-class Resources:
-    """
-    Provides a single object to hold shared application resources. 
-
-    Resources are expressed as either string or resource instance. If string,
-    class name is expressed as "module:class"; for example:
-    "myapp.resources.v1:FooResource". Resources classes expressed as strings
-    must have __init__ methods that take no arguments other than "self".
-    Resource classes expressed as string values are lazily imported and
-    instantiated at the time of first access; this solves potential circular
-    dependencies between resources.
-
-    Resources are exposed as object attributes. Example initialization:
-
-    resources = Resources({
-        "foo": "myapp.resources.v1:FooResource",
-        "bar": "myapp.resources.v1:BarResource",
-        "qux": qux_instance,
-    })
-
-    Resources can be accessed as either attributes or items, like:
-    resources.foo and resources["foo"].
-    """
-
-    def __init__(self, resources={}):
-        super().__init__()
-        self._roax_lock = threading.Lock()
-        self._roax_resources = {}
-        for name, value in resources.items():
-            setattr(self, name, value)
-
-    def __getattr__(self, name):
-        try:
-            return self[name]
-        except KeyError:
-            raise AttributeError(
-                f"{self.__class__.__name__} object has no attribute '{name}'"
-            )
-
-    def __setattr__(self, name, value):
-        if name.startswith("_roax_") or name in super().__dir__():
-            super().__setattr__(name, value)
-        else:
-            self[name] = value
-
-    def __delattr__(self, name):
-        if name.startswith("_roax_") or name in super().__dir__():
-            super().__delattr__(name)
-        else:
-            try:
-                del self[name]
-            except KeyError:
-                raise AttributeError(name)
-
-    def __dir__(self):
-        return [*super().__dir__(), *iter(self._roax_resources)]
-
-    def __len__(self):
-        return len(self._roax_resources)
-
-    def __getitem__(self, key):
-        return self._roax_resolve(key)
-
-    def __setitem__(self, key, value):
-        if not isinstance(value, str) and not isinstance(value, Resource):
-            raise TypeError("resource must be str or Resource type")
-        if isinstance(value, str) and ("." not in value or ":" not in value):
-            raise ValueError("resource class name must be fully qualified")
-        self._roax_resources[key] = value
-
-    def __delitem__(self, key):
-        del self._roax_resources[key]
-
-    def __iter__(self):
-        return iter(self._roax_resources)
-
-    def __contains__(self, item):
-        return item in self._roax_resources
-
-    def _roax_resolve(self, key):
-        resources = self._roax_resources
-        if isinstance(resources[key], str):
-            with self._roax_lock:
-                if isinstance(resources[key], str):
-                    mod, cls = resources[key].split(":")
-                    resources[key] = getattr(importlib.import_module(mod), cls)()
-        return resources[key]
+    detail, code = "Internal Server Error", 500
 
 
 def _summary(function):
@@ -249,24 +107,25 @@ def _summary(function):
     return " ".join(result)
 
 
-def authorize(security):
+async def authorize(security):
     """
-    Peform authorization of the operation.
-    
-    This function executes all of the security requirements associated with
-    the operation. If a single security requirement does not raise a
-    security exception {Unauthorized, Forbidden}, then this function passes
-    and authorization is granted.
+    Peform authorization of an operation.
 
-    If all security requirements raised a security exception: if one security
-    requirement raised Forbidden, then Forbidden is raised; otherwise
-    Unauthorized is raised. If a non-security exception is raised, then
-    it is re-raised by this function.
+    Parameters:
+    • security: Iterable of security requirements.
+
+    This coroutine executes the security requirements. If any security
+    requirement does not raise an exception then this coroutine passes and
+    authorization is granted.
+
+    If one security requirement raises a Forbidden exception, then a Forbidden
+    exception will be raised; otherwise an Unauthorized exception will be
+    raised. If a non-security exception is raised, then it is re-raised.
     """
     exception = None
     for requirement in security or []:
         try:
-            requirement.authorize()
+            await requirement.authorize()
             return  # security requirement authorized the operation
         except Forbidden:
             exception = Forbidden
@@ -276,86 +135,145 @@ def authorize(security):
         except:
             raise
     if exception:
-        raise exception()
+        raise exception
 
 
 def _params(function):
     sig = inspect.signature(function)
-    return roax.schema.dict(
+    return schema.dict(
         props={k: v for k, v in function.__annotations__.items() if k != "return"},
         required={p.name for p in sig.parameters.values() if p.default is p.empty},
     )
 
 
+class _Descriptor:
+    def __init__(self, **kwargs):
+        for key in kwargs:
+            setattr(self, key, kwargs[key])
+
+
+def resource(wrapped=None, *, tag=None):
+    """
+    Decorate a class to be a resource containing operations.
+
+    Parameters:
+    • tag: Tag to group resources.  [resource class name in lower case]
+    """
+
+    if wrapped is None:
+        return functools.partial(resource, tag=tag)
+
+    operations = {}
+    for name in dir(wrapped):
+        attr = getattr(wrapped, name)
+        if operation := getattr(attr, "_fondat_operation", None):
+            operations[name] = operation
+    wrapped._fondat_resource = _Descriptor(
+        tag=tag or wrapped.__name__.lower(), operations=operations,
+    )
+    return wrapped
+
+
+_methods = {
+    "get": "query",
+    "put": "mutation",
+    "post": "mutation",
+    "delete": "mutation",
+    "patch": "mutation",
+}
+_types = {"query": "get", "mutation": "post", "link": "get"}
+
+
 def operation(
-    _fn=None,
+    wrapped=None,
     *,
-    name=None,
     type=None,
-    summary=None,
-    description=None,
+    method=None,
     security=None,
     publish=True,
     deprecated=False,
 ):
     """
-    Decorate a resource method to register it as a resource operation.
+    Decorate a resource coroutine to register it as an operation.
 
     Parameters:
-    • name: Operation name. Required if the operation type is "query" or "action".
-    • type: Type of operation being registered.  {"create", "read", "update", "delete", "action", "query", "patch"}
-    • summary: Short summary of what the operation does.
-    • description: Verbose description of the operation.  [function docstring]
+    • type: Type of operation.  {"query", "mutation", "link"}
+    • method: Method type of operation.  {"get", "put", "post", "delete", "patch"}
     • security: Security requirements for the operation.
     • publish: Publish the operation in documentation.
     • deprecated: Declare the operation as deprecated.
+
+    The method intentionally aligns with standard HTTP methods:
+    {"get", "put", "post", "delete", "patch"}. If an operation is named the
+    same as one of these method types without explicitly specifying a method
+    for the operation, it will be assigned that method type.
+
+    The default method for query operations is "get"; the default method
+    for mutation operations is "post". This should not ordinarily be
+    overridden.
     """
 
-    _valid_types = {"create", "read", "update", "delete", "query", "action", "patch"}
-
-    def decorator(function):
-        _name = name
-        _type = type
-        _description = description or function.__doc__ or function.__name__
-        __summary = summary or _summary(function)
-        if _name is None:
-            _name = function.__name__
-        if _type is None and _name in {"create", "read", "update", "delete", "patch"}:
-            _type = _name
-        if _type not in _valid_types:
-            raise ValueError(f"operation type must be one of: {_valid_types}")
-
-        def wrapper(wrapped, instance, args, kwargs):
-            operation = wrapped.__self__.operations[wrapped.__name__]
-            tags = {"operation": operation.name, "resource": operation.resource.name}
-            with context.push({"context": "roax.operation", **tags}):
-                with roax.monitor.timer({"name": "operation_duration_seconds", **tags}):
-                    with roax.monitor.counter(
-                        {"name": "operation_calls_total", **tags}
-                    ):
-                        authorize(operation.security)
-                        return wrapped(*args, **kwargs)
-
-        decorated = roax.schema.validate(wrapt.decorator(wrapper)(function))
-        operation = dict(
-            function=function.__name__,
-            name=_name,
-            type=_type,
-            summary=__summary,
-            description=_description,
-            params=_params(function),
-            returns=function.__annotations__.get("return"),
+    if wrapped is None:
+        return functools.partial(
+            operation,
+            type=type,
             security=security,
             publish=publish,
             deprecated=deprecated,
         )
-        try:
-            getattr(function, "__self__")._register_operation(**operation)
-        except AttributeError:  # not yet bound to an instance
-            function._roax_operation_ = operation  # __init__ will register it
-        return decorated
 
-    if callable(_fn):
-        return decorator(_fn)
-    else:
-        return decorator
+    if not asyncio.iscoroutinefunction(wrapped):
+        raise TypeError("operation must be a coroutine")
+
+    name = wrapped.__name__
+    description = wrapped.__doc__ or name
+    summary = _summary(wrapped)
+
+    _method = method or (name if name in _methods else None)
+    _type = type or (_methods.get(_method))
+    _method = _method or _types.get(_type)
+
+    if _type is None:
+        raise ValueError(f"unknown operation type: {type}")
+    if _type not in _types:
+        raise ValueError(f"invalid operation type: {type}")
+    if _method is None:
+        raise ValueError(f"unknown operation method: {method}")
+    if _method not in _methods:
+        raise ValueError(f"invalid operation method: {method}")
+
+    @wrapt.decorator
+    async def wrapper(wrapped, instance, args, kwargs):
+        operation = getattr(wrapped, "_fondat_operation")
+        tags = {
+            "resource": f"{instance.__class__.__module__}.{instance.__class__.__qualname__}",
+            "operation": wrapped.__name__,
+        }
+        with context.push({"context": "fondat.operation", **tags}):
+            async with monitor.timer({"name": "operation_duration_seconds", **tags}):
+                async with monitor.counter({"name": "operation_calls_total", **tags}):
+                    await authorize(operation.security)
+                    return await wrapped(*args, **kwargs)
+
+    wrapped._fondat_operation = _Descriptor(
+        name=name,
+        type=_type,
+        method=_method,
+        summary=summary,
+        description=description,
+        security=security,
+        publish=publish,
+        deprecated=deprecated,
+        params=_params(wrapped),
+        returns=wrapped.__annotations__.get("return"),
+    )
+
+    return schema.validate(wrapper(wrapped))
+
+
+def get_operations(resource):
+    """Return dict of name-to-descriptor for resource operation methods."""
+    try:
+        return resource._fondat_resource.operations
+    except AttributeError:
+        return {}
--- tests/test_resource.py
+++ tests/test_resource.py
@@ -1,102 +1,90 @@
+import fondat.schema as s
 import pytest
-import roax.schema as s
 import uuid
 
-from roax.resource import Resource, Resources, ResourceError, operation
+from fondat.resource import (
+    ResourceError,
+    resource,
+    operation,
+    get_operations,
+)
 
 
 body_schema = s.dict({"id": s.uuid(), "foo": s.str()}, "foo")
 
 
-class R1(Resource):
+@resource
+class R2:
+    def __init__(self, id):
+        self.id = id
+
+    @operation
+    async def delete():
+        pass
+
+
+@resource
+class R1:
 
     schema = body_schema
 
     @operation  # no params
-    def create(self, body: body_schema) -> s.uuid():
+    async def post(self, body: body_schema) -> s.uuid():
         return uuid.UUID("705d9048-97d6-4071-8359-3dbf0531fee9")
 
     @operation()  # empty params
-    def delete(self, id: s.uuid()):
+    async def delete(self, id: s.uuid()):
         pass
 
     @operation(type="query")
-    def foo(self) -> s.str():
+    async def foo(self) -> s.str():
+        return "foo"
+
+
+@resource
+class R3(R1):
+    @operation(type="query")
+    async def bar(self) -> s.str():
         return "bar"
 
 
-class R3(Resource):
-    @operation(type="query")
-    def qux(self) -> s.str():
-        return "baz"
+@pytest.mark.asyncio
+async def test_call():
+    await R1().post(body={"foo": "bar"})
 
 
-class InvalidOperationTypeResource(Resource):
-    def __init__(self):
-        super().__init__()
-        self.not_a_valid_operation_type = operation()(self.not_a_valid_operation_type)
-
-    def not_a_valid_operation_type(self):
-        pass
+def test_operations():
+    assert get_operations(R1).keys() == {"post", "delete", "foo"}
+    assert get_operations(R1()).keys() == {"post", "delete", "foo"}
 
 
-def test_r1_operations():
-    r1 = R1()
-    assert r1.operations["create"].name == "create"
-    assert r1.operations["foo"].type == "query"
+def test_inheritance():
+    assert get_operations(R3).keys() == {"post", "delete", "foo", "bar"}
 
 
-def test_call():
-    result = R1().operations["create"].call(body={"foo": "bar"})
+@pytest.mark.asyncio
+async def test_invalid_schema():
+    with pytest.raises(s.SchemaError):
+        await R1().post(body=1)
 
 
 def test_invalid_operation_type():
     with pytest.raises(ValueError):
-        InvalidOperationTypeResource()
+
+        class MissingOperationTypeResource:
+            @operation
+            async def missing_operation_type(self):
+                pass
 
 
-def test_override_operation_type():
-    class OverrideOperationTypeResource(Resource):
-        @operation(type="create")
-        def not_a_valid_operation_type(self):
-            pass
-
-
-def test_resources():
-    resources = Resources({"r1": f"{R1.__module__}:R1", "r3": R3()})
-    assert len(resources) == 2
-    assert "r3" in dir(resources)
-    assert set(iter(resources)) == {"r1", "r3"}
-    assert resources.r1.foo() == "bar"
-    assert resources["r3"].qux() == "baz"
-    assert "r3" in resources
-    del resources["r3"]
-    assert "r3" not in resources
-    resources["r3"] = R3()
-    assert resources.r3.qux() == "baz"
-    del resources.r3
-    assert "r3" not in resources
-    resources.r3 = R3()
-    assert resources["r3"].qux() == "baz"
-    del resources.r3
-    assert len(resources) == 1
-    assert "r1" in dir(resources)
-    assert "r3" not in dir(resources)
-    resources._roax_test = "test"
-    del resources._roax_test
-    with pytest.raises(AttributeError):
-        del resources._roax_test
-    with pytest.raises(TypeError):
-        resources["int"] = 1
+def test_mismatch_operation_type():
     with pytest.raises(ValueError):
-        resources["nodots"] = "no_dots_or_colons_in_this_string"
-    with pytest.raises(KeyError):
-        del resources["r3"]
-    with pytest.raises(AttributeError):
-        resources.r3
-    with pytest.raises(AttributeError):
-        del resources.r3
+
+        class InvalidOperationTypeResource:
+            @operation(type="postit")
+            async def not_post(self):
+                pass
 
 
 def test_resource_error():
-    assert str(ResourceError("foo")) == "foo"
+    assert str(ResourceError("foo", 123)) == "foo"
