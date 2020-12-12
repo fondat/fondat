69c4796098ada204bf7438dff2d4c57c95cf1c51
Paul Bryan <pbryan@anode.ca>
2020-12-12 11:22:43 -0800

    New resource idiom.

--- fondat/memory.py
+++ fondat/memory.py
@@ -1,104 +1,136 @@
 """Module to store resource items in memory."""
 
+from __future__ import annotations
+
 import collections
+import collections.abc
 import copy
 import datetime
-import fondat.schema as s
+import fondat.security
 import threading
-import wrapt
+import typing
 
-from fondat.resource import resource, operation, link, NotFound, BadRequest
+from fondat.resource import resource, operation, NotFound, BadRequest
+from fondat.typing import affix_type_hints
 
 
 _now = lambda: datetime.datetime.now(tz=datetime.timezone.utc)
 
+_delta = lambda s: datetime.timedelta(seconds=s)
+
 _Item = collections.namedtuple("Item", "data,time")
 
-_Oldest = collections.namedtuple("Oldest", "id,time")
+_Oldest = collections.namedtuple("Oldest", "key,time")
 
 
-def memory_resource(schema, size=None, evict=False, ttl=None, security=None):
+def memory_resource(
+    key_type: type,
+    value_type: type,
+    size: int = None,
+    evict: bool = False,
+    ttl: int = None,
+    security: collections.abc.Iterable[fondat.security.SecurityRequirement] = None,
+):
     """
     Return a new resource class that stores items in a dictionary.
 
     Parameters:
-    • schema: Schema of items.
+    • key_type: Type for the key for each item.
+    • value_type: Type for value stored in each item.
     • size: Maximum number of items to store.  [unlimited]
-    • evict: Should oldest item be evicted to make room for a new item. 
+    • evict: Should oldest item be evicted to make room for a new item.
     • ttl: Maximum item time to live, in seconds.  [unlimited]
     • security: Security requirements to apply to all operations.
     """
 
     @resource
-    class MemoryResource:
-
-        @resource
-        class Item:
-
-            def __init__(self, mr, id):
-                self._mr = mr
-                self.id = id
-
-            @operation(security=security)
-            async def get(self) -> schema:
-                """Read item."""
-                item = self._mr._items.get(self.id)
-                if item is None or (self._mr._ttl and _now() > item.time + self._mr._ttl):
-                    raise NotFound
-                return item.data
-
-            @operation(security=security)
-            async def put(self, data: schema):
-                """Write item."""
-                with self._mr._lock:
-                    now = _now()
-                    if self._mr._ttl:  # purge expired entries; pay the price on puts
-                        for id in {
-                            k for k, v in self._mr._items.items() if v.time + self._mr._ttl <= now
-                        }:
-                            del self._mr._items[self.id]
-                    while evict and size and len(self._mr._items) >= size:  # evict oldest entry
-                        oldest = None
-                        for _id, _item in self._mr._items.items():
-                            if not oldest or _item.time < oldest.time:
-                                oldest = _Oldest(_id, _item.time)
-                        if oldest:
-                            del self._mr._items[oldest.id]
-                    if size and len(self._mr._items) >= size:
-                        raise BadRequest("item size limit reached")
-                    self._mr._items[self.id] = _Item(copy.deepcopy(data), now)
-
-            @operation(security=security)
-            async def delete(self):
-                """Delete item."""
-                await self.get()  # ensure item exists and has not expired
-                with self._mr._lock:
-                    self._mr._items.pop(self.id, None)
-
+    class Container:
         def __init__(self):
-            self._items = {}
-            self._ttl = datetime.timedelta(seconds=ttl) if ttl else None
-            self._lock = threading.Lock()
+            self.storage = {}
+            self.lock = threading.Lock()
+
+        def __getitem__(self, key: key_type) -> Item:
+            return Item(self, key)
 
         @operation(security=security)
-        async def get(self) -> s.list(s.str()):
-            """Return list of item identifiers."""
+        async def get(self) -> list[key_type]:
+            """Return list of item keys."""
             now = _now()
-            with self._lock:
+            with self.lock:
                 return [
-                    id
-                    for id, item in self._items.items()
-                    if not self._ttl or item.time + self._ttl <= now
+                    key
+                    for key, item in self.storage.items()
+                    if not self.ttl or item.time + _delta(self.ttl) <= now
                 ]
 
         @operation(type="mutation", security=security)
-        async def clear(self):
+        async def clear(self) -> None:
             """Remove all items."""
-            with self._lock:
-                self._items.clear()
+            with self.lock:
+                self.storage.clear()
 
-        @link
-        def __getitem__(self, id: s.str()) -> Item:
-            return MemoryResource.Item(self, id)
+    Container.key_type = key_type
+    Container.value_type = value_type
+    Container.size = size
+    Container.evict = evict
+    Container.ttl = ttl
 
-    return MemoryResource
+    @resource
+    class Item:
+        def __init__(self, container: Container, key: key_type):
+            self.container = container
+            self.key = key
+
+        @operation(security=security)
+        async def get(self) -> value_type:
+            """Read item."""
+            item = self.container.storage.get(self.key)
+            if item is None or (
+                self.container.ttl and _now() > item.time + _delta(self.container.ttl)
+            ):
+                raise NotFound
+            return item.data
+
+        @operation(security=security)
+        async def put(self, data: value_type) -> None:
+            """Write item."""
+            with self.container.lock:
+                now = _now()
+                if self.container.ttl:  # purge expired entries; pay the price on puts
+                    for key in {
+                        k
+                        for k, v in self.container.storage.items()
+                        if v.time + _delta(container.ttl) <= now
+                    }:
+                        del self.container.storage[self.key]
+                while (
+                    self.container.evict
+                    and self.container.size
+                    and len(self.container.storage) >= self.container.size
+                ):  # evict oldest entry
+                    oldest = None
+                    for _key, _item in self.container.storage.items():
+                        if not oldest or _item.time < oldest.time:
+                            oldest = _Oldest(_key, _item.time)
+                    if oldest:
+                        del self.container.storage[oldest.key]
+                if (
+                    self.container.size
+                    and len(self.container.storage) >= self.container.size
+                ):
+                    raise BadRequest("item size limit reached")
+                self.container.storage[self.key] = _Item(copy.deepcopy(data), now)
+
+        @operation(security=security)
+        async def delete(self):
+            """Delete item."""
+            await self.get()  # ensure item exists and has not expired
+            with self.container.lock:
+                self.container.storage.pop(self.key, None)
+
+    affix_type_hints(Container, localns=locals())
+    affix_type_hints(Item, localns=locals())
+
+    Container.__name__ = Container.__qualname__ = "MemoryResource"
+
+    return Container
--- tests/test_memory.py
+++ tests/test_memory.py
@@ -1,9 +1,9 @@
 import fondat.resource as r
-import fondat.schema as s
 import pytest
 
+from dataclasses import make_dataclass
 from fondat.memory import memory_resource
-from fondat.resource import BadRequest, Conflict, NotFound, operation, link
+from fondat.resource import BadRequest, Conflict, NotFound, operation
 from time import sleep
 from uuid import uuid4
 
@@ -12,89 +12,89 @@ pytestmark = pytest.mark.asyncio
 
 
 async def test_gpdl_dict():
-    schema = s.dict({"foo": s.str(), "bar": s.int()}, "foo bar")
-    mr = memory_resource(schema)()
+    DC = make_dataclass("DC", [("foo", str), ("bar", int)])
+    resource = memory_resource(key_type=str, value_type=DC)()
     id = "id1"
-    r1 = {"foo": "hello", "bar": 1}
-    await mr[id].put(r1)
-    r2 = await mr[id].get()
+    r1 = DC("hello", 1)
+    await resource[id].put(r1)
+    r2 = await resource[id].get()
     assert r1 == r2
-    r1["bar"] = 2
-    await mr[id].put(r1)
-    r2 = await mr[id].get()
+    r1.bar = 2
+    await resource[id].put(r1)
+    r2 = await resource[id].get()
     assert r1 == r2
-    await mr[id].delete()
-    assert await mr.get() == []
+    await resource[id].delete()
+    assert await resource.get() == []
 
 
 async def test_gpdl_str():
-    mr = memory_resource(s.str())()
+    resource = memory_resource(key_type=str, value_type=str)()
     data = "你好，世界!"
     id = "hello_world"
-    await mr[id].put(data)
-    assert await mr.get() == [id]
-    assert await mr[id].get() == data
+    await resource[id].put(data)
+    assert await resource.get() == [id]
+    assert await resource[id].get() == data
     data = "Goodbye world!"
-    await mr[id].put(data)
-    assert await mr[id].get() == data
-    await mr[id].delete()
-    assert await mr.get() == []
+    await resource[id].put(data)
+    assert await resource[id].get() == data
+    await resource[id].delete()
+    assert await resource.get() == []
 
 
 async def test_gpdl_bytes():
-    mr = memory_resource(s.bytes())()
+    resource = memory_resource(key_type=str, value_type=bytes)()
     data = b"\x00\x0e\0x01\0x01\0x00"
     id = "binary"
-    await mr[id].put(data)
-    assert await mr.get() == [id]
-    assert await mr[id].get() == data
+    await resource[id].put(data)
+    assert await resource.get() == [id]
+    assert await resource[id].get() == data
     data = bytes((1, 2, 3, 4, 5))
-    await mr[id].put(data)
-    assert await mr[id].get() == data
-    await mr[id].delete()
-    assert await mr.get() == []
+    await resource[id].put(data)
+    assert await resource[id].get() == data
+    await resource[id].delete()
+    assert await resource.get() == []
 
 
 async def test_get_notfound():
-    mr = memory_resource(s.str())()
+    resource = memory_resource(key_type=str, value_type=str)()
     with pytest.raises(NotFound):
-        await mr["1"].get()
+        await resource["1"].get()
 
 
 async def test_delete_notfound():
-    mr = memory_resource(s.str())()
+    resource = memory_resource(key_type=str, value_type=str)()
     with pytest.raises(NotFound):
-        await mr["1"].delete()
+        await resource["1"].delete()
 
 
 async def test_clear():
-    mr = memory_resource(s.str())()
-    await mr["1"].put("foo")
-    await mr["2"].put("bar")
-    assert len(await mr.get()) == 2
-    await mr.clear()
-    assert len(await mr.get()) == 0
+    resource = memory_resource(key_type=str, value_type=str)()
+    await resource["1"].put("foo")
+    await resource["2"].put("bar")
+    assert len(await resource.get()) == 2
+    await resource.clear()
+    assert len(await resource.get()) == 0
 
 
 async def test_size_limit():
-    mr = memory_resource(s.str(), size=1)()
-    await mr["1"].put("foo")
+    resource = memory_resource(key_type=str, value_type=str, size=1)()
+    await resource["1"].put("foo")
     with pytest.raises(BadRequest):
-        await mr["2"].put("bar")
+        await resource["2"].put("bar")
 
 
 async def test_size_evict():
-    mr = memory_resource(s.str(), size=2, evict=True)()
-    await mr["1"].put("foo")
-    await mr["2"].put("bar")
-    await mr["3"].put("qux")
-    assert set(await mr.get()) == {"2", "3"}
+    resource = memory_resource(key_type=str, value_type=str, size=2, evict=True)()
+    await resource["1"].put("foo")
+    await resource["2"].put("bar")
+    await resource["3"].put("qux")
+    assert set(await resource.get()) == {"2", "3"}
 
 
 async def test_ttl():
-    mr = memory_resource(s.str(), ttl=0.1)()
-    await mr["1"].put("foo")
-    await mr["1"].get()
+    resource = memory_resource(key_type=str, value_type=str, ttl=0.1)()
+    await resource["1"].put("foo")
+    await resource["1"].get()
     sleep(0.2)
     with pytest.raises(NotFound):
-        await mr["1"].get()
+        await resource["1"].get()
