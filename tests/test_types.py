6a8e661d78d97194ed63601d24bfbf4eafff7d5c
Paul Bryan <pbryan@anode.ca>
2020-12-17 20:03:31 -0800

    Cleanup.

--- tests/test_types.py
+++ tests/test_types.py
@@ -1,31 +1,31 @@
 import pytest
 
-import fondat.enum
+from fondat.types import str_enum
 
 
 def test_str_enum_iterable():
-    E = fondat.enum.str_enum("E", ("a", "b", "c"))
+    E = str_enum("E", ("a", "b", "c"))
     assert E.A == "a"
     assert E.B == "b"
     assert E.C == "c"
 
 
 def test_str_enum_str_spaces():
-    E = fondat.enum.str_enum("E", "a b c")
+    E = str_enum("E", "a b c")
     assert E.A == "a"
     assert E.B == "b"
     assert E.C == "c"
 
 
 def test_str_enum_str_commas():
-    E = fondat.enum.str_enum("E", "a,b,c")
+    E = str_enum("E", "a,b,c")
     assert E.A == "a"
     assert E.B == "b"
     assert E.C == "c"
 
 
 def test_str_enum_str_mixed():
-    E = fondat.enum.str_enum("E", "a,,  b,c")
+    E = str_enum("E", "a,,  b,c")
     assert E.A == "a"
     assert E.B == "b"
     assert E.C == "c"
