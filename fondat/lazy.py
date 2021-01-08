323bd450d5afefbc5ad7372809edcc18fa0a8118
Paul Bryan <pbryan@anode.ca>
2021-01-07 16:46:52 -0800

    Add lazy initialization module.

---
+++ fondat/lazy.py
@@ -0,0 +1,118 @@
+"""Fondat lazy evaluation module."""
+
+import importlib
+import threading
+
+from collections.abc import Callable
+from typing import Any
+
+
+class LazyMap:
+    """
+    A map-like object of key-value pairs, in which a value can be lazily
+    initialized at the time of first access. This is useful to allow resources
+    to access other resources, preventing circular dependencies. 
+
+    Parameters:
+    • init: Mapping of key-value pairs to initialize the lazy map.
+
+    To lazily initialize a value, set it to a no-argument function callback
+    that has been decorated with the @lazy decorator. When the value is then
+    first accessed, the callback will be called to initialize the value.
+    The resulting value will then be stored in the mapping.
+
+    All map values can be set, retrieved and deleted as items, like: map["key"]
+    and attributes, like: map.key.
+    """
+
+    def __init__(self, init=None):
+        super().__init__()
+        self._fondat_lock = threading.Lock()
+        self._fondat_map = {}
+        if init is not None:
+            for name, value in init.items():
+                self[name] = value
+
+    def __getattr__(self, name):
+        try:
+            return self[name]
+        except KeyError:
+            raise AttributeError(
+                f"{self.__class__.__name__} object has no attribute '{name}'"
+            )
+
+    def __setattr__(self, name, value):
+        if name.startswith("_fondat_") or name in super().__dir__():
+            super().__setattr__(name, value)
+        else:
+            self[name] = value
+
+    def __delattr__(self, name):
+        if name.startswith("_fondat_") or name in super().__dir__():
+            super().__delattr__(name)
+        else:
+            try:
+                del self[name]
+            except KeyError:
+                raise AttributeError(name)
+
+    def __dir__(self):
+        return [*super().__dir__(), *iter(self._fondat_map)]
+
+    def __len__(self):
+        return len(self._fondat_map)
+
+    def __getitem__(self, key):
+        return self._fondat_resolve(key)
+
+    def __setitem__(self, key, value):
+        self._fondat_map[key] = value
+
+    def __delitem__(self, key):
+        del self._fondat_map[key]
+
+    def __iter__(self):
+        return iter(self._fondat_map)
+
+    def __contains__(self, item):
+        return item in self._fondat_map
+
+    def _fondat_resolve(self, key):
+        value = self._fondat_map[key]
+        if is_lazy(value):
+            with self._fondat_lock:
+                if is_lazy(value):  # prevent race
+                    self._fondat_map[key] = value()
+            value = self._fondat_map[key]
+        return value
+
+
+def lazy(function: Callable) -> Callable:
+    """
+    Decorate a function to tag it as a lazy initializer.
+    """
+    if not callable(function):
+        raise TypeError("function must be callable")
+    setattr(function, "_fondat_lazy", True)
+    return function
+
+
+def lazy_attr(module_name: str, attr_name: str) -> Callable:
+    """
+    Return a lazy callback function to import a module and return an attribute.
+
+    Parameters:
+    • module_name: The name of the module to import.
+    • attr_name: The name of the attribute in the module to be returned.
+    """
+    def callback():
+        return getattr(importlib.import_module(module_name), attr_name)
+
+    return lazy(callback)
+
+
+def is_lazy(obj: Any) -> bool:
+    """
+    Return True if an object is a callable is tagged as a lazy callback function.
+    """
+    return callable(obj) and getattr(obj, "_fondat_lazy", None)
---
+++ tests/test_lazy.py
@@ -0,0 +1,54 @@
+import pytest
+
+from fondat.lazy import LazyMap, lazy
+
+
+@lazy
+def lz1():
+    return "lz1_value"
+
+
+@lazy
+def lz2():
+    return "lz2_value"
+
+
+def not_decorated():
+    return "bacon"
+
+
+def test_lazy():
+    map = LazyMap({"a": "a_value", "z1": lz1,})
+    assert len(map) == 2
+    assert "a" in map
+    assert "z1" in map
+    assert "z2" not in map
+    assert map["a"] == "a_value"
+    assert map.a == "a_value"
+    assert map["z1"] == "lz1_value"
+    assert map.z1 == "lz1_value"
+    map.a = "a1"
+    assert map.a == "a1"
+    assert map["a"] == "a1"
+    map.z1 = "lz1v"
+    assert map.z1 == "lz1v"
+    assert map["z1"] == "lz1v"
+    map.z1 = lz1
+    assert map.z1 == "lz1_value"
+    map["z1"] = not_decorated
+    assert map.z1 == not_decorated
+    map.z2 = lz2
+    assert map["z2"] == "lz2_value"
+    assert len(map) == 3
+    del map.z1
+    assert len(map) == 2
+    del map["z2"]
+    assert len(map) == 1
+    with pytest.raises(AttributeError):
+        del map.not_in_map
+    with pytest.raises(KeyError):
+        del map["nobody_home"]
+    with pytest.raises(AttributeError):
+        map.not_here
+    del map.a
+    assert len(map) == 0
