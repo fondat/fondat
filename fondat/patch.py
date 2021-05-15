86da44818c65dfea742ef74d8673643af09362cc
Paul Bryan <pbryan@anode.ca>
2021-05-14 17:51:19 -0700

    Apply merge patch on JSON representations of any type.

--- fondat/patch.py
+++ fondat/patch.py
@@ -3,15 +3,11 @@
 import collections.abc
 import copy
 
+from fondat.codec import get_codec, JSON
+from typing import Any
 
-def merge_patch(target, patch):
-    """
-    Apply JSON Merge Patch document, per RFC 7386.
 
-    Parameters:
-    • target: JSON value to mutate
-    • patch: patch document to apply
-    """
+def _json_merge_patch(target, patch):
     if isinstance(patch, collections.abc.Mapping):
         if not isinstance(target, collections.abc.Mapping):
             target = {}
@@ -19,7 +15,21 @@ def merge_patch(target, patch):
             if value is None:
                 target.pop(key, None)
             else:
-                target[key] = merge_patch(target.get(key), value)
+                target[key] = _json_merge_patch(target.get(key), value)
         return target
     else:
         return copy.deepcopy(patch)
+
+
+def json_merge_patch(*, value: Any, type: type = Any, patch: Any) -> Any:
+    """
+    Return a new patched value, the result of applying a JSON Merge Patch document to the JSON
+    representation of the specified value, per RFC 7386.
+
+    Parameters:
+    • value: value to be patched
+    • type: type of value to be patched
+    • patch: JSON Merge Patch document to apply to value
+    """
+    codec = get_codec(JSON, type)
+    return codec.decode(_json_merge_patch(codec.encode(value), patch))
--- tests/test_patch.py
+++ tests/test_patch.py
@@ -1,112 +1,65 @@
 import pytest
 
-from fondat.patch import merge_patch
+
+from dataclasses import make_dataclass, field
+from fondat.patch import json_merge_patch
+from typing import Optional
 
 
-def _test_patch(original, patch, result):
-    assert merge_patch(original, patch) == result
+def test_merge_patch_rfc_7386_test_cases():
+    assert json_merge_patch(value={"a": "b"}, patch={"a": "c"}) == {"a": "c"}
+    assert json_merge_patch(value={"a": "b"}, patch={"b": "c"}) == {"a": "b", "b": "c"}
+    assert json_merge_patch(value={"a": "b"}, patch={"a": None}) == {}
+    assert json_merge_patch(value={"a": "b", "b": "c"}, patch={"a": None}) == {"b": "c"}
+    assert json_merge_patch(value={"a": ["b"]}, patch={"a": "c"}) == {"a": "c"}
+    assert json_merge_patch(value={"a": "c"}, patch={"a": ["b"]}) == {"a": ["b"]}
+    assert json_merge_patch(value={"a": {"b": "c"}}, patch={"a": {"b": "d", "c": None}}) == {
+        "a": {"b": "d"}
+    }
+    assert json_merge_patch(value={"a": [{"b": "c"}]}, patch={"a": [1]}) == {"a": [1]}
+    assert json_merge_patch(value=["a", "b"], patch=["c", "d"]) == ["c", "d"]
+    assert json_merge_patch(value={"a": "b"}, patch=["c"]) == ["c"]
+    assert json_merge_patch(value={"a": "foo"}, patch=None) == None
+    assert json_merge_patch(value={"a": "foo"}, patch="bar") == "bar"
+    assert json_merge_patch(value={"e": None}, patch={"a": 1}) == {"e": None, "a": 1}
+    assert json_merge_patch(value=[1, 2], patch={"a": "b", "c": None}) == {"a": "b"}
+    assert json_merge_patch(value={}, patch={"a": {"bb": {"ccc": None}}}) == {"a": {"bb": {}}}
 
 
-def test_merge_patch_01():
-    original = {"a": "b"}
-    patch = {"a": "c"}
-    result = {"a": "c"}
-    _test_patch(original, patch, result)
+def test_merge_patch_dc_flat_success():
+    DC = make_dataclass("DC", (("a", str),))
+    assert json_merge_patch(type=DC, value=DC(a="b"), patch={"a": "c"}) == DC(a="c")
 
 
-def test_merge_patch_02():
-    original = {"a": "b"}
-    patch = {"b": "c"}
-    result = {"a": "b", "b": "c"}
-    _test_patch(original, patch, result)
+def test_merge_patch_dc_flat_error():
+    DC = make_dataclass("DC", (("a", str),))
+    with pytest.raises(TypeError):
+        json_merge_patch(type=DC, value=DC(a="b"), patch={"a": 1})
 
 
-def test_merge_patch_03():
-    original = {"a": "b"}
-    patch = {"a": None}
-    result = {}
-    _test_patch(original, patch, result)
+def test_merge_patch_dc_flat_optional():
+    DC = make_dataclass("DC", (("a", str), ("b", Optional[str], field(default=None))))
+    assert json_merge_patch(type=DC, value=DC(a="b"), patch={"b": "c"}) == DC(a="b", b="c")
 
 
-def test_merge_patch_04():
-    original = {"a": "b", "b": "c"}
-    patch = {"a": None}
-    result = {"b": "c"}
-    _test_patch(original, patch, result)
+def test_merge_patch_dc_flat_delete():
+    DC = make_dataclass("DC", (("a", str), ("b", Optional[str], field(default=None))))
+    assert json_merge_patch(type=DC, value=DC(a="b", b="c"), patch={"b": None}) == DC(
+        a="b", b=None
+    )
 
 
-def test_merge_patch_05():
-    original = {"a": ["b"]}
-    patch = {"a": "c"}
-    result = {"a": "c"}
-    _test_patch(original, patch, result)
+def test_merge_patch_dc_nested_add():
+    DC2 = make_dataclass("DC2", (("b", str),))
+    DC1 = make_dataclass("DC1", (("a", DC2),))
+    assert json_merge_patch(
+        type=DC1, value=DC1(a=DC2(b="c")), patch={"a": {"b": "d", "c": None}}
+    ) == DC1(a=DC2(b="d"))
 
 
-def test_merge_patch_06():
-    original = {"a": "c"}
-    patch = {"a": ["b"]}
-    result = {"a": ["b"]}
-    _test_patch(original, patch, result)
-
-
-def test_merge_patch_07():
-    original = {"a": {"b": "c"}}
-    patch = {"a": {"b": "d", "c": None}}
-    result = {"a": {"b": "d"}}
-    _test_patch(original, patch, result)
-
-
-def test_merge_patch_08():
-    original = {"a": [{"b": "c"}]}
-    patch = {"a": [1]}
-    result = {"a": [1]}
-    _test_patch(original, patch, result)
-
-
-def test_merge_patch_09():
-    original = ["a", "b"]
-    patch = ["c", "d"]
-    result = ["c", "d"]
-    _test_patch(original, patch, result)
-
-
-def test_merge_patch_10():
-    original = {"a": "b"}
-    patch = ["c"]
-    result = ["c"]
-    _test_patch(original, patch, result)
-
-
-def test_merge_patch_11():
-    original = {"a": "foo"}
-    patch = None
-    result = None
-    _test_patch(original, patch, result)
-
-
-def test_merge_patch_12():
-    original = {"a": "foo"}
-    patch = "bar"
-    result = "bar"
-    _test_patch(original, patch, result)
-
-
-def test_merge_patch_13():
-    original = {"e": None}
-    patch = {"a": 1}
-    result = {"e": None, "a": 1}
-    _test_patch(original, patch, result)
-
-
-def test_merge_patch_14():
-    original = [1, 2]
-    patch = {"a": "b", "c": None}
-    result = {"a": "b"}
-    _test_patch(original, patch, result)
-
-
-def test_merge_patch_15():
-    original = {}
-    patch = {"a": {"bb": {"ccc": None}}}
-    result = {"a": {"bb": {}}}
-    _test_patch(original, patch, result)
+def test_merge_patch_dc_nested_delete():
+    DC2 = make_dataclass("DC2", (("b", Optional[str]),))
+    DC1 = make_dataclass("DC1", (("a", DC2),))
+    assert json_merge_patch(type=DC1, value=DC1(a=DC2(b="c")), patch={"a": {"b": None}}) == DC1(
+        a=DC2(b=None)
+    )
