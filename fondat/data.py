3c24d0f04d71e1a36ccb95005b23e86d2c4205f8
Paul Bryan <pbryan@anode.ca>
2021-05-19 15:15:12 -0700

    Dataclass function additions.

--- fondat/data.py
+++ fondat/data.py
@@ -5,7 +5,7 @@ import typing
 
 from collections.abc import Iterable
 from fondat.types import is_optional
-from typing import Union
+from typing import Any, Optional, Union
 
 
 class _MISSING:
@@ -70,21 +70,97 @@ def datacls(cls: type, init: bool = True, **kwargs) -> type:
 
 def make_datacls(
     cls_name: str,
-    fields: Iterable[Union[str, tuple[str, type], tuple[str, type, dataclasses.Field]]],
+    fields: Iterable[
+        Union[str, tuple[str, type], tuple[str, type, dataclasses.Field], dataclasses.Field]
+    ],
     init: bool = True,
     **kwargs,
 ) -> type:
     """
-    Return a new dynamically created dataclass. This decorator wraps the Python
-    dataclasses.make_dataclass function, with the following changes to the generated __init__
-    method:
+    Return a new dataclass. This decorator wraps the Python dataclasses.make_dataclass
+    function, with the following changes to the generated __init__ method:
 
     • fields (with default values or not) can be declared in any order
+    • fields also accepts the result of the dataclasses.fields() function
     • the __init__ method only accepts keyword arguments
     • Optional[...] fields default to None if no default value is specified
     """
 
-    dc = dataclasses.make_dataclass(cls_name, fields, init=False, **kwargs)
+    result = dataclasses.make_dataclass(
+        cls_name=cls_name,
+        fields=tuple(
+            (f.name, f.type, f) if isinstance(f, dataclasses.Field) else f for f in fields
+        ),
+        init=False,
+        **kwargs,
+    )
     if init:
-        dc.__init__ = _datacls_init(dc)
-    return dc
+        result.__init__ = _datacls_init(result)
+    return result
+
+
+def flds(dataclass: Any, names: set[str] = None) -> tuple[dataclasses.Field]:
+    """
+    Return a tuple describing the fields of a dataclass.
+
+    Parameters:
+    • dataclass: dataclass or dataclass instance containing fields to describe
+    • names: the names of the dataclass fields to describe
+    """
+
+    fields = {field.name: field for field in dataclasses.fields(dataclass)}
+    if names is not None:
+        fields = {name: fields[name] for name in names}
+    return tuple(fields.values())
+
+
+def subset_datacls(
+    cls_name: str,
+    dataclass: type,
+    names: Iterable[str] = None,
+    optional: Union[bool, Iterable[str]] = False,
+    **kwargs,
+) -> type:
+    """
+    Return a new dataclass, containing a subset of fields from another dataclass.
+
+    • dataclass: source dataclass
+    • names: the names of source dataclass fields to include
+    • optional: fields to be made optional
+
+    If names are not specified, then all fields from the source dataclass are included.
+
+    If optional is a boolean value, it specifies if all fields should be made optional.
+    """
+
+    if names is None:
+        names = dataclass.__annotations__.keys()
+    fields = flds(dataclass, names)
+    optional = set(names if optional is True else () if optional is False else optional)
+    for field in fields:
+        if field.name in optional:
+            field.type = Optional[field.type]
+    return make_datacls(cls_name, fields=fields, **kwargs)
+
+
+def copy(source: Any, target: Any, fields: Iterable[str] = None):
+    """
+    Copy fields from one instance of a dataclass to another.
+
+    Parameters:
+    • source: instance to copy fields from
+    • target: instance to copy fields to
+    • fields: names of fields to be copied
+
+    If fields are specified, then only those fields shall be copied; they must be present
+    in both source and target dataclass instances. If fields are not specified, then only
+    fields that source and target dataclasses have in common will be copied.
+
+    This function makes no attempt to ensure type compatibility between dataclass fields.
+    """
+
+    if fields is None:
+        fields = source.__annotations__.keys() & target.__annotations__.keys()
+
+    for field in fields:
+        setattr(target, field, getattr(source, field))
--- tests/test_data.py
+++ tests/test_data.py
@@ -1,6 +1,7 @@
 import pytest
 
-from fondat.data import datacls, make_datacls
+from fondat.data import copy, datacls, flds, make_datacls, subset_datacls
+from fondat.types import is_optional
 from dataclasses import field
 from typing import Optional
 
@@ -57,3 +58,72 @@ def test_make_datacls_field_default_factory():
     Foo = make_datacls("Foo", (("x", dict, field(default_factory=dict)),))
     foo = Foo()
     assert foo.x == {}
+
+
+def test_datacls_flds_subset():
+    Foo = make_datacls("Foo", (("a", int), ("b", str), ("c", float)))
+    subset = {"a", "b"}
+    fields = flds(Foo, subset)
+    assert set([f.name for f in fields]) == subset
+    Bar = make_datacls("Bar", fields)
+    assert Bar.__annotations__.keys() == subset
+
+
+def test_subset_datacls():
+    Foo = make_datacls("Foo", (("a", int), ("b", str), ("c", float)))
+    subset = {"a", "b"}
+    Bar = subset_datacls("Bar", Foo, subset)
+    assert Bar.__annotations__.keys() == subset
+
+
+def test_subset_datacls():
+    Foo = make_datacls("Foo", (("a", int), ("b", str), ("c", float)))
+    Bar = subset_datacls("Bar", Foo)
+    assert Bar.__annotations__.keys() == Foo.__annotations__.keys()
+
+
+def test_subset_datacls_optional_true():
+    Foo = make_datacls("Foo", (("a", int), ("b", str), ("c", float)))
+    Bar = subset_datacls("Bar", Foo, optional=True)
+    for field in flds(Bar):
+        assert is_optional(field.type)
+
+
+def test_subset_dataclass_optional_subset():
+    Foo = make_datacls("Foo", (("a", int), ("b", str), ("c", float)))
+    optional = {"a", "b"}
+    Bar = subset_datacls("Bar", Foo, optional=optional)
+    for f in flds(Bar):
+        assert (f.name in optional and is_optional(f.type)) or not is_optional(f.type)
+
+
+def test_copy_full():
+    Foo = make_datacls(
+        "Foo", (("a", Optional[int]), ("b", Optional[str]), ("c", Optional[float]))
+    )
+    foo = Foo(a=1, b="a", c=2.0)
+    bar = Foo()
+    copy(foo, bar)
+    assert foo == bar
+
+
+def test_copy_subset():
+    Foo = make_datacls(
+        "Foo", (("a", Optional[int]), ("b", Optional[str]), ("c", Optional[float]))
+    )
+    foo = Foo(a=1, b="a", c=2.0)
+    bar = Foo()
+    subset = {"a", "b"}
+    copy(foo, bar, subset)
+    for name in bar.__annotations__:
+        assert getattr(bar, name) == (getattr(foo, name) if name in subset else None)
+
+
+def test_copy_common():
+    Foo = make_datacls("Foo", (("a", int), ("b", str), ("c", float)))
+    Bar = make_datacls("Bar", (("a", Optional[int]), ("b", Optional[str])))
+    foo = Foo(a=1, b="a", c=2.0)
+    bar = Bar()
+    copy(foo, bar)
+    for name in bar.__annotations__:
+        assert getattr(bar, name) == getattr(foo, name)
