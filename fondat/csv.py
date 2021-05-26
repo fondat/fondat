01fb4a644fab19ec5256757c79b60c4ab4973214
Paul Bryan <pbryan@anode.ca>
2021-05-26 09:29:55 -0700

    Add dataclass/row encoding/decoding.

--- fondat/csv.py
+++ fondat/csv.py
@@ -7,13 +7,13 @@ to a specific decimal precision.
 """
 
 import csv
+import dataclasses
 import io
 
-from collections.abc import Callable, Iterable
-from dataclasses import dataclass
-from decimal import Decimal
-from fondat.codec import get_codec, String
-from typing import Any, Optional
+from collections.abc import Iterable, Mapping
+from fondat.codec import Codec, String, get_codec
+from fondat.types import is_optional
+from typing import Any, Optional, get_type_hints
 
 
 def _round(value: Any, precision: Optional[int]) -> str:
@@ -23,98 +23,166 @@ def _round(value: Any, precision: Optional[int]) -> str:
             value = value.rstrip("0").rstrip(".")
         return value
     if precision == 0:
-        return str(int(round(value)))
+        return str(round(value))
     return f"{{:.{precision}f}}".format(value)
 
 
-def currency_encoder(
-    prefix: str = "", suffix: str = "", precision: int = None
-) -> Callable[[Any], str]:
+def currency_codec(python_type: Any, prefix: str = "", suffix: str = "", precision: int = None):
     """
-    Return a function that encodes a number as a currency value.
+    Return a codec that encodes/decodes a number as a currency value; optionally encodes with
+    fixed-point precision.
 
     Parameters:
+    • python_type: type of the value to be encoded/decoded
     • prefix: currency prefix (e.g. "$")
     • suffix: currency suffix
-    • precision: round values to precise precision  [floating point]
+    • precision: round encoded value to number of digits  [floating point]
     """
 
-    def encode(value: Any) -> str:
-        return f"{prefix}{_round(value, precision)}{suffix}" if value is not None else ""
+    codec = get_codec(String, python_type)
 
-    return encode
+    class CurrencyCodec(String[python_type]):
+        def encode(self, value: python_type) -> str:
+            return f"{prefix}{_round(value, precision)}{suffix}" if value is not None else ""
+
+        def decode(self, value: str) -> python_type:
+            result = codec.decode(value.lstrip(prefix).rstrip(suffix))
+            if precision is not None:
+                result = round(result, precision)
+            return result
+
+    return CurrencyCodec()
 
 
-def percent_encoder(precision: int = None) -> Callable[[Any], str]:
+def percent_codec(python_type: Any, precision: int):
     """
-    Return a function that encodes fractional value as a percentage.
+    Return a codec that encodes/decodes a fractional value as a percentage string with
+    fixed-point precision.
 
     Parameter:
-    • precision: round values to precise precision  [floating point]
+    • python_type: type of the value to be encoded/decoded
+    • precision: round encoded value to number of digits
     """
 
-    def encode(value: Any) -> str:
-        if value is None:
-            return ""
-        value = str(_round(Decimal(str(value)) * 100, precision))
-        return f"{value}%"
+    codec = get_codec(String, python_type)
 
-    return encode
+    class PercentCodec(String[python_type]):
+        def encode(self, value: python_type) -> str:
+            return f"{_round(value * 100, precision)}%"
+
+        def decode(self, value: str) -> python_type:
+            result = codec.decode(value.rstrip("%")) / 100
+            if precision is not None:
+                result = round(result, precision + 2)
+            return result
+
+    return PercentCodec()
 
 
-def number_encoder(precision: int = None) -> Callable[[Any], str]:
+def fixed_codec(python_type: Any, precision: int):
     """
-    Return a function that encodes a numeric value.
+    Return a codec encodes/decodes a number with fixed-point precision.
 
     Parameter:
-    • precision: round values to precise precision  [floating point]
+    • python_type: type of the value to be encoded/decoded
+    • precision: round encoded value to number of digits
     """
 
-    def encode(value: Any) -> str:
-        return _round(value, precision) if value is not None else ""
+    codec = get_codec(String, python_type)
 
-    return encode
+    class FixedCodec(String[python_type]):
+        def encode(self, value: Any) -> str:
+            return _round(value, precision) if value is not None else ""
+
+        def decode(self, value: str) -> python_type:
+            result = codec.decode(value)
+            if precision is not None:
+                result = round(result, precision)
+            return result
+
+    return FixedCodec()
 
 
-class DataclassWriter:
+def dataclass_row_codec(
+    dataclass: Any,
+    columns: Iterable[str] = None,
+    fields: Mapping[str, str] = None,
+    codecs: Mapping[str, Any] = None,
+):
     """
-    Writes dataclass instances as CSV rows.
-
-    Each dataclass attribute becomes a column in a row. An attribute is encoded by either a
-    supplied encoder, or with an appropriate string codec.
+    Return a codec that encodes/decodes a dataclass to/from a CSV row.
 
     Parameters:
-    • fileobj: writeable file-like object to write CSV into
-    • dataclass: dataclass defining columns in CSV row
-    • encoders: mapping of attribute names to encoder to encode it in CSV
-    • dialect: string, type or instance for dialect of CSV file being written
+    • dataclass: dataclass type to encode/decode
+    • columns: ordered column names
+    • fields: column-to-field mappings
+    • codecs: column-to-codec mappings
+
+    A row is list of strings.
+
+    The columns parameter specifies the names of CSV columns, and the order they are encoded
+    in a row. If the columns parameter is omitted, then columns will be all dataclass
+    fields, in the order they are defined in the dataclass.
+
+    The fields mapping specifies which database fields map to which columns. If no mapping for
+    a given column is specified, then the column name will match the field name.
+
+    The codecs mapping specifies which codecs are used to encode columns. If no mapping for a
+    given column is provided, then the default codec for its associated field is used.
     """
 
-    __slots__ = ("_writer", "_dataclass", "_encoders")
+    if not dataclasses.is_dataclass(dataclass):
+        raise TypeError("dataclass parameter must be a dataclass")
 
-    def __init__(
-        self,
-        fileobj: Any,
-        dataclass: type,
-        encoders: dict[str, Callable] = None,
-        dialect: Any = "excel",
-    ):
-        self._writer = csv.writer(fileobj, dialect)
-        self._dataclass = dataclass
-        self._encoders = {
-            name: encoders.get(name, get_codec(String, dataclass.__annotations__[name]).encode)
-            for name in dataclass.__annotations__
-        }
+    hints = get_type_hints(dataclass, include_extras=True)
 
-    def write_header(self) -> None:
-        self._writer.writerow(name for name in self._dataclass.__annotations__)
+    if columns is None:
+        columns = tuple(key for key in hints.keys())
 
-    def write_row(self, row: Any) -> None:
-        columns = (
-            self._encoders[name](getattr(row, name)) for name in self._dataclass.__annotations__
-        )
-        self._writer.writerow(columns)
+    if fields is None:
+        fields = {field: field for field in hints}
 
-    def write_rows(self, rows: Iterable[Any]) -> None:
-        for row in rows:
-            self.write_row(row)
+    fields = {column: field for column, field in fields.items() if column in columns}
+
+    if codecs is None:
+        codecs = {}
+
+    codecs = {
+        column: codecs.get(column, get_codec(String, hints[fields[column]]))
+        for column in columns
+        if column in fields
+    }
+
+    optional_fields = {field for field in fields if is_optional(hints[field])}
+
+    class DataclassRowCodec(Codec[dataclass, list[str]]):
+        """Encodes/decodes a dataclass to/from a CSV row."""
+
+        def encode(self, value: dataclass) -> list[str]:
+            """
+            Encode from dataclass to row.
+
+            If a field value is None, it will be represented in a column as an empty string.
+            """
+            return [codecs[column].encode(getattr(value, fields[column])) for column in columns]
+
+        def decode(self, values: list[str]) -> dataclass:
+            """
+            Decode from row to dataclass.
+
+            If a column to decode contains an empty string value, it will be represented as
+            None if the associated field is optional.
+            """
+            kwargs = {}
+            for column, value in zip(columns, values):
+                field = fields.get(column)
+                if not field:  # ignore unmapped column
+                    continue
+                hint = hints[field]
+                if value == "" and field in optional_fields:
+                    kwargs[field] = None
+                else:
+                    kwargs[field] = codecs[column].decode(value)
+            return dataclass(**kwargs)
+
+    return DataclassRowCodec()
--- tests/test_csv.py
+++ tests/test_csv.py
@@ -6,108 +6,185 @@ import io
 from dataclasses import make_dataclass
 from datetime import date
 from decimal import Decimal
+from typing import Optional
 
 
-def test_currency_floating_prefix():
-    encode = fondat.csv.currency_encoder(prefix="$")
-    assert encode(123) == "$123"
-    assert encode(123.45) == "$123.45"
-    assert encode(678.90) == "$678.9"
-    assert encode(Decimal("678.90")) == "$678.9"
+def _test_codec(codec, python_value, string_value):
+    assert codec.encode(python_value) == string_value
+    assert codec.decode(string_value) == python_value
 
 
-def test_currency_floating_suffix():
-    encode = fondat.csv.currency_encoder(suffix=" лв")
-    assert encode(123) == "123 лв"
-    assert encode(123.45) == "123.45 лв"
-    assert encode(678.90) == "678.9 лв"
-    assert encode(Decimal("678.90")) == "678.9 лв"
+def test_int_currency_floating_prefix():
+    codec = fondat.csv.currency_codec(int, prefix="$")
+    _test_codec(codec, 123, "$123")
 
 
-def test_currency_fixed2_prefix():
-    encode = fondat.csv.currency_encoder(prefix="$", precision=2)
-    assert encode(123) == "$123.00"
-    assert encode(123.45) == "$123.45"
-    assert encode(678.9012345) == "$678.90"
-    assert encode(Decimal("678.9012345")) == "$678.90"
+def test_float_currency_floating_prefix():
+    codec = fondat.csv.currency_codec(float, prefix="$")
+    _test_codec(codec, 123.45, "$123.45")
+    _test_codec(codec, 678.9, "$678.9")
 
 
-def test_currency_fixed2_suffix():
-    encode = fondat.csv.currency_encoder(suffix=" лв", precision=2)
-    assert encode(123) == "123.00 лв"
-    assert encode(123.45) == "123.45 лв"
-    assert encode(678.9012345) == "678.90 лв"
-    assert encode(Decimal("678.9012345")) == "678.90 лв"
+def test_decimal_currency_floating_prefix():
+    codec = fondat.csv.currency_codec(Decimal, prefix="$")
+    _test_codec(codec, Decimal("678.90"), "$678.9")
 
 
-def test_currency_fixed0_prefix():
-    encode = fondat.csv.currency_encoder(prefix="$", precision=0)
-    assert encode(123) == "$123"
-    assert encode(123.45) == "$123"
-    assert encode(678.90) == "$679"
-    assert encode(Decimal("678.90")) == "$679"
+def test_int_currency_suffix():
+    codec = fondat.csv.currency_codec(int, suffix=" лв")
+    _test_codec(codec, 123, "123 лв")
 
 
-def test_currency_fixed0_suffix():
-    encode = fondat.csv.currency_encoder(suffix=" лв", precision=0)
-    assert encode(123) == "123 лв"
-    assert encode(123.45) == "123 лв"
-    assert encode(678.90) == "679 лв"
-    assert encode(Decimal("678.90")) == "679 лв"
+def test_float_currency_floating_suffix():
+    codec = fondat.csv.currency_codec(float, suffix=" лв")
+    _test_codec(codec, 123.45, "123.45 лв")
+    _test_codec(codec, 678.9, "678.9 лв")
 
 
-def test_percentage_float():
-    encode = fondat.csv.percent_encoder()
-    assert encode(0.123) == "12.3%"
-    assert encode(0.001234) == "0.1234%"
-    assert encode(1.234) == "123.4%"
+def test_decimal_currency_floating_suffix():
+    codec = fondat.csv.currency_codec(Decimal, suffix=" лв")
+    _test_codec(codec, Decimal("678.90"), "678.9 лв")
 
 
-def test_percentage_fixed2():
-    encode = fondat.csv.percent_encoder(precision=2)
-    assert encode(0.123) == "12.30%"
-    assert encode(0.001234) == "0.12%"
-    assert encode(1.234) == "123.40%"
+def test_float_currency_fixed2_prefix():
+    codec = fondat.csv.currency_codec(float, prefix="$", precision=2)
+    _test_codec(codec, 123.45, "$123.45")
+    _test_codec(codec, 678.9, "$678.90")
+    assert codec.encode(678.9012345) == "$678.90"
 
 
-def test_percentage_fixed0():
-    encode = fondat.csv.percent_encoder(precision=0)
-    assert encode(0.123) == "12%"
-    assert encode(0.001234) == "0%"
-    assert encode(1.235) == "124%"
+def test_decimal_currency_prec2_prefix():
+    codec = fondat.csv.currency_codec(Decimal, prefix="$", precision=2)
+    _test_codec(codec, Decimal("678.9"), "$678.90")
+    assert codec.encode(Decimal("678.9012345")) == "$678.90"
 
 
-def test_number_float():
-    encode = fondat.csv.number_encoder()
-    assert encode(123) == "123"
-    assert encode(123.45) == "123.45"
-    assert encode(678.90) == "678.9"
-    assert encode(Decimal("678.90")) == "678.9"
+def test_float_currency_prec2_suffix():
+    codec = fondat.csv.currency_codec(float, suffix=" лв", precision=2)
+    _test_codec(codec, 123.45, "123.45 лв")
+    assert codec.encode(678.9012345) == "678.90 лв"
 
 
-def test_number_fixed2():
-    encode = fondat.csv.number_encoder(precision=2)
-    assert encode(123) == "123.00"
-    assert encode(123.45) == "123.45"
-    assert encode(678.90) == "678.90"
-    assert encode(Decimal("678.90")) == "678.90"
+def test_decimal_currency_prec2_suffix():
+    codec = fondat.csv.currency_codec(Decimal, suffix=" лв", precision=2)
+    _test_codec(codec, Decimal("678.9"), "678.90 лв")
+    assert codec.encode(Decimal("678.9012345")) == "678.90 лв"
 
 
-def test_number_fixed2():
-    encode = fondat.csv.number_encoder(precision=0)
-    assert encode(123) == "123"
-    assert encode(123.45) == "123"
-    assert encode(678.90) == "679"
-    assert encode(Decimal("678.90")) == "679"
+def test_int_currency_prec0_prefix():
+    codec = fondat.csv.currency_codec(int, prefix="$", precision=0)
+    _test_codec(codec, 123, "$123")
 
 
-def test_dataclass():
+def test_float_currency_prec0_prefix():
+    codec = fondat.csv.currency_codec(float, prefix="$", precision=0)
+    _test_codec(codec, 123.0, "$123")
+    assert codec.encode(123.45) == "$123"
+    assert codec.encode(678.90) == "$679"
+
+
+def test_decimal_currency_prec0_prefix():
+    codec = fondat.csv.currency_codec(Decimal, prefix="$", precision=0)
+    _test_codec(codec, Decimal("123.0"), "$123")
+    assert codec.encode(Decimal("678.90")) == "$679"
+
+
+def test_int_currency_prec0_suffix():
+    codec = fondat.csv.currency_codec(int, suffix=" лв", precision=0)
+    _test_codec(codec, 123, "123 лв")
+
+
+def test_float_currency_prec0_suffix():
+    codec = fondat.csv.currency_codec(float, suffix=" лв", precision=0)
+    _test_codec(codec, 123.0, "123 лв")
+    assert codec.encode(123.45) == "123 лв"
+    assert codec.encode(678.90) == "679 лв"
+
+
+def test_decimal_currency_prec0_suffix():
+    codec = fondat.csv.currency_codec(Decimal, suffix=" лв", precision=0)
+    _test_codec(codec, Decimal("123.0"), "123 лв")
+    assert codec.encode(Decimal("678.90")) == "679 лв"
+
+
+def test_percentage_prec2():
+    codec = fondat.csv.percent_codec(float, 2)
+    _test_codec(codec, 0.123, "12.30%")
+    _test_codec(codec, 1.234, "123.40%")
+    assert codec.encode(0.001234) == "0.12%"
+
+
+def test_percentage_prec0():
+    codec = fondat.csv.percent_codec(float, 0)
+    _test_codec(codec, 0.12, "12%")
+    assert codec.encode(0.123) == "12%"
+    assert codec.encode(0.001234) == "0%"
+    assert codec.encode(1.235) == "124%"
+
+
+def test_float_fixed_prec2():
+    codec = fondat.csv.fixed_codec(float, 2)
+    _test_codec(codec, 123.45, "123.45")
+    _test_codec(codec, 678.9, "678.90")
+
+
+def test_decimal_fixed_prec2():
+    codec = fondat.csv.fixed_codec(Decimal, 2)
+    _test_codec(codec, Decimal("678.90"), "678.90")
+
+
+def test_float_fixed_prec0():
+    codec = fondat.csv.fixed_codec(Decimal, 0)
+    _test_codec(codec, 123.00, "123")
+    assert codec.encode(123.45) == "123"
+    assert codec.encode(678.90) == "679"
+
+
+def test_decimal_fixed_prec0():
+    codec = fondat.csv.fixed_codec(Decimal, 0)
+    _test_codec(codec, Decimal("123.00"), "123")
+    assert codec.encode(Decimal("678.90")) == "679"
+
+
+def test_encode():
     DC = make_dataclass("DC", (("x", int), ("y", float), ("z", date)))
-    with io.StringIO() as s:
-        dcw = fondat.csv.DataclassWriter(s, DC, {"x": fondat.csv.number_encoder(precision=2)})
-        dcw.write_header()
-        assert s.getvalue() == "x,y,z\r\n"
-        s.seek(0)
-        s.truncate()
-        dcw.write_row(DC(1, 2.34, date(2021, 3, 2)))
-        assert s.getvalue() == "1.00,2.34,2021-03-02\r\n"
+    codecs = {"y": fondat.csv.percent_codec(int, 2)}
+    dcc = fondat.csv.dataclass_row_codec(dataclass=DC, codecs=codecs)
+    assert dcc.encode(DC(x=1, y=0.123, z=date(2021, 3, 2))) == ["1", "12.30%", "2021-03-02"]
+
+
+def test_encode_none():
+    DC = make_dataclass("DC", (("x", Optional[str]), ("y", str)))
+    dcc = fondat.csv.dataclass_row_codec(dataclass=DC)
+    assert dcc.encode(DC(x=None, y="")) == ["", ""]
+
+
+def test_decode_simple():
+    DC = make_dataclass("DC", (("x", int), ("y", float), ("z", date)))
+    dcc = fondat.csv.dataclass_row_codec(dataclass=DC)
+    assert dcc.decode(["1", "12.35", "2021-03-02"]) == DC(x=1, y=12.35, z=date(2021, 3, 2))
+
+
+def test_decode_reordered():
+    DC = make_dataclass("DC", (("x", int), ("y", float), ("z", date)))
+    dcc = fondat.csv.dataclass_row_codec(dataclass=DC, columns=["z", "x", "y"])
+    assert dcc.decode(["2021-03-02", "1", "12.35"]) == DC(x=1, y=12.35, z=date(2021, 3, 2))
+
+
+def test_decode_codec():
+    DC = make_dataclass("DC", (("x", str), ("y", float)))
+    codecs = {"y": fondat.csv.currency_codec(float, "$")}
+    dcc = fondat.csv.dataclass_row_codec(dataclass=DC, codecs=codecs)
+    assert dcc.decode(["foo", "$123.45"]) == DC(x="foo", y=123.45)
+
+
+def test_decode_ignore():
+    DC = make_dataclass("DC", (("a", str), ("b", str)))
+    dcc = fondat.csv.dataclass_row_codec(dataclass=DC, columns=["a", "c", "b", "d"])
+    assert dcc.decode(["a", "c", "b", "d"]) == DC(a="a", b="b")
+
+
+def test_decode_empty_columns():
+    DC = make_dataclass("DC", (("a", Optional[str]), ("b", str)))
+    dcc = fondat.csv.dataclass_row_codec(dataclass=DC, columns=["a", "b", "c"])
+    assert dcc.decode(["", ""]) == DC(a=None, b="")
