cfdf4c56810b2059475de9ee02482b2b20185fb0
Paul Bryan <pbryan@anode.ca>
2022-01-18 21:47:46 -0800

    Add tests.

---
+++ tests/test_sql.py
@@ -0,0 +1,15 @@
+import pytest
+
+from fondat.sql import Expression
+
+
+def test_expression_params():
+    expr = Expression("1", "2")
+    assert str(expr) == "12"
+
+
+def test_expression_join():
+    exprs = []
+    for n in range(2):
+        exprs.append(Expression.join((Expression(n) for n in range(3)), "."))
+    assert str(Expression.join(exprs, ":")) == "0.1.2:0.1.2"
